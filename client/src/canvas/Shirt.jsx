cad2fc68bf4e36136b771d12b55b139d9ea335c0
w-lucas-hagan <w.lucas.hagan@gmail.com>
2023-09-21 13:49:10 -0400

    -- inital implementation of camera and 3D rotation

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -16,7 +16,7 @@ const Shirt = () => {
 return (
     <group>
         <mesh castShadow geometry={nodes.T_Shirt_male.geometry} material={materials.lambert1} material-roughness={1} dispose={null}>
-
+            
         </mesh>
     </group>
   )
