d237c1bf19f4e06b5183c59446a4ed21fac96b69
w-lucas-hagan <w.lucas.hagan@gmail.com>
2023-09-21 16:35:35 -0400

    - complete DALL.E setup     - send prompt from front end to server     - handle prompt     - return a response     -send response to front end

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -18,4 +18,25 @@ router.route('/').get((req, res) => {
     res.status(200).json({message: "Hello from DALL.E ROUTES"})
 })
 
+// router to pass prompt from front end to server
+router.route('/').post(async(req, res) => {
+    try {
+        // get prompt from front end 
+        const { prompt } = req.body;
+
+        // wait for server response (creation of image)
+        const response = await openai.createImage({prompt, n: 1, size: '1024x1024', response_format: 'b64_json'})
+
+        // store image from server response 
+        const image = response.data.data[0].b64_json
+
+        // send image to front end
+        res.status(200).json({ photo: image})
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Something went wrong!"})
+    }
+})
+
 export default router;
