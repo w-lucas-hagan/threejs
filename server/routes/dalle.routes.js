fd89984b369b89395ea1de2fcea4630dae408b53
w-lucas-hagan <w.lucas.hagan@gmail.com>
2023-09-21 16:25:45 -0400

    - added secret key

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -8,6 +8,11 @@ dotenv.config()
 // creating a router 
 const router = express.Router()
 
+// setting secret key to call AI image generation
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
 // routes
 router.route('/').get((req, res) => {
     res.status(200).json({message: "Hello from DALL.E ROUTES"})
